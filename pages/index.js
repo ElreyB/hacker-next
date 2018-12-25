f453f612b03562c8a9df51b122d5cd3da9fa2876
Elrey Belmonti <elrey.belmonti@formidable.com>
2018-12-25 14:57:44 -0800

    handleing errors wit built in error page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,40 @@
+import fetch from 'isomorphic-fetch';
+import Error from 'next/error';
+import styled from 'styled-components';
+
+const Title = styled.h1``;
+
 class Index extends React.Component {
+  static async getInitialProps() {
+    let stories;
+    try {
+      const responds = await fetch(
+        'https://node-hnapi.herokuapp.com/news?page=1'
+      );
+      stories = await responds.json();
+    } catch (err) {
+      console.log(err);
+      stories = [];
+    }
+    return {
+      stories
+    };
+  }
   render() {
-    return <div>Hacker NExt</div>;
+    const { stories } = this.props;
+    if (stories.length === 0) {
+      return <Error statusCode={502} />;
+    }
+    return (
+      <div>
+        <Title>Hacker Next</Title>
+        <div>
+          {stories.map(story => (
+            <h2 key={story.id}>{story.title}</h2>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
